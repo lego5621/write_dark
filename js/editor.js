84a5b3c8b6395053c48904e0b492cee9107367f8
Oleg <lego5621@gmail.com>
2020-05-30 14:39:16 +0600

    chenge admin redactor

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,31 @@
-var quill = new Quill('#editor', {
-    theme: 'snow'
+var save = function (context) {
+  var ui = $.summernote.ui;
+
+  // create button
+  var button = ui.button({
+    contents: '<i/> Save',
+    tooltip: 'Save shange',
+    click: function () {
+      // invoke insertText method with 'hello' on editor module.
+      context.invoke('editor.insertText', 'hello');
+    }
   });
+
+  return button.render();   // return button as jquery object
+}
+
+$(document).ready(function() {
+  $('#summernote').summernote({height: 600,toolbar: [
+    ['style', ['style']],
+    ['font', ['bold', 'underline','strikethrough', 'superscript', 'subscript' ]],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['table', ['table']],
+    ['insert', ['link', 'picture']],
+    ['clear'],
+    ['mybutton', ['save']]
+  ],
+  buttons: {
+    save: save
+  }});
+});
+
